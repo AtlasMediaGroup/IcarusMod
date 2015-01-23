e0c23080fc7717acbaf993db52d69683f0b66c80
credmore99@gmail.com <>
2015-01-23 22:16:19 +0000

    Fixing my github thing

--- src/com/superiornetworks/icarus/bridge/CoreProtect.java
+++ src/com/superiornetworks/icarus/bridge/CoreProtect.java
@@ -1,9 +1,24 @@
 package com.superiornetworks.icarus.bridge;
 
+import net.coreprotect.CoreProtectAPI;
 
 public class CoreProtect
 {
+
+    private static CoreProtectAPI getCoreProtect()
+    {
+        CoreProtectAPI CoreProtect = CoreProtectAPI.plugin.getAPI();
+        return CoreProtect;
+    }
     
-    // Another blank file. Just so that the folder actually shows on github and ready for some code madness!
+    public static void rollback(String user, int time, int radius)
+    {
+        getCoreProtect().performRollback(user, time, radius, null, null, null);
+    }
+    
+    public static void restore(String user, int time, int radius)
+    {
+        getCoreProtect().performRestore(user, time, radius, null, null, null);
+    }
     
 }
