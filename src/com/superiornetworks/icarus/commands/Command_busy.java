116acb7a674cb4d5172d735b41600f66f679ba4d
Scuph <hentscheljr1@aol.com>
2015-05-23 13:31:16 -0400

    Update Command_busy.java

--- src/com/superiornetworks/icarus/commands/Command_busy.java
+++ src/com/superiornetworks/icarus/commands/Command_busy.java
@@ -6,7 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandParameters(name="busy",description="Toggle busy mode.",usage="/busy",rank=ICM_Rank.Rank.SUPER)
+@CommandParameters(name="busy",description="Toggle your busy status to on/off.",usage="/busy",rank=ICM_Rank.Rank.SUPER)
 public class Command_busy
 {
     public boolean onCommand(CommandSender sender, Command cmnd, String string, String[] args)
