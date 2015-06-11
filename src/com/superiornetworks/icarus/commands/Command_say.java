32c781ff63c0dfc7772b5eccc55b5c90b21a4527
hockeyfan360 <hockeyskater19@gmail.com>
2015-06-10 20:05:39 -0500

    Adds a smite command. Closes #48.

--- src/com/superiornetworks/icarus/commands/Command_say.java
+++ src/com/superiornetworks/icarus/commands/Command_say.java
@@ -1,13 +1,15 @@
 package com.superiornetworks.icarus.commands;
 
 import com.superiornetworks.icarus.ICM_Rank;
-import com.superiornetworks.icarus.ICM_Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-@CommandParameters(name="say",description="Say something formal with this.",usage="/say <message>",rank=ICM_Rank.Rank.SUPER)
+@CommandParameters(name="smite",description="The most basic form of disciplinary action.",usage="/smite <player>",rank=ICM_Rank.Rank.SUPER)
 public class Command_say
 {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
@@ -16,8 +18,38 @@ public class Command_say
         {
             return false;
         }
-        String message = ICM_Utils.buildMessage(args, 0);
-        Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server:" + sender.getName() + "] " + message);
+        Player player = Bukkit.getServer().getPlayer(args[0]);
+        if (player == null)
+        {
+            sender.sendMessage(ChatColor.DARK_RED + "Player not found!");
+            return true;
+        }
+        
+        if (player.getName().equals(sender.getName()))
+        {
+            sender.sendMessage(ChatColor.DARK_RED + "Please do not attempt to smite yourself.");
+            return true;
+        }
+        
+        if (ICM_Rank.getRank(sender).level <= ICM_Rank.getRank(player).level)
+        {
+            sender.sendMessage(ChatColor.DARK_RED + "You cannot smite someone that is equal or higher in ranking than yourself.");
+            return true;
+        }
+        
+        World world = player.getWorld();
+        world.strikeLightningEffect(player.getLocation());
+        world.strikeLightningEffect(player.getLocation());
+        world.strikeLightningEffect(player.getLocation());
+        world.strikeLightningEffect(player.getLocation());
+        world.strikeLightningEffect(player.getLocation());
+        world.strikeLightningEffect(player.getLocation());
+        player.getInventory().clear();
+        player.setHealth(0D);
+        player.setOp(false);
+        player.setGameMode(GameMode.SURVIVAL);
+        Bukkit.broadcastMessage(ChatColor.RED + player.getName() + " has been very naughty, indeed.");
+        player.sendMessage(ChatColor.RED + "You were smitten by: " + sender.getName());
         return true;
     }
 }
