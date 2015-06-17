a33d46e64176396b23178cae0e95f791c6cb408c
Paul19988 <paul.ba1rd@hotmail.com>
2015-06-17 16:35:07 +0100

    Tab API now Usable

--- src/space/paulcodes/otherapis/TabAPI.java
+++ src/space/paulcodes/otherapis/TabAPI.java
@@ -1,4 +1,12 @@
 package space.paulcodes.otherapis;
 
+import org.bukkit.entity.Player;
+
 public class TabAPI {
+
+    public static void setPlayerTab(String prefix, Player p) {
+        p.setCustomName(prefix + p.getName());
+        p.setPlayerListName(p.getCustomName());
+    }
+
 }
