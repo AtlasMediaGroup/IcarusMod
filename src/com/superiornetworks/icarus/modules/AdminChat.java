2bf1c2f5da70edad34e01d6d4bf5e67b2e811f57
Chaosiity (Git) <hentscheljr1@aol.com>
2016-04-13 16:51:06 -0400

    Implement AdminChat Module and Command
    Solves #173

---
+++ src/com/superiornetworks/icarus/modules/AdminChat.java
@@ -0,0 +1,28 @@
+package com.superiornetworks.icarus.modules;
+
+import com.superiornetworks.icarus.ICM_Rank;
+import com.superiornetworks.icarus.IcarusMod;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AdminChat extends IcarusModule
+{
+
+    public AdminChat(IcarusMod plugin)
+    {
+        super(plugin);
+    }
+
+    public static void AdminChatMsg(CommandSender sender, String message)
+    {
+        for (Player onlineadmins : Bukkit.getOnlinePlayers())
+        {
+            if (ICM_Rank.isRankOrHigher(onlineadmins, ICM_Rank.Rank.SUPER))
+            {
+                // Some strange ass colour codes being used here...
+                onlineadmins.sendMessage("§f[§bADMIN§f] §c" + sender.getName() + ICM_Rank.getRank(sender).actag + "§f: §b " + message);
+            }
+        }
+    }
+}
