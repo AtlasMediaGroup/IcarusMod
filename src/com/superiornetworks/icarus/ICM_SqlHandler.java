49e2e7536b76801bae50669449846cf1be128a2a
Chaosiity (Git) <hentscheljr1@aol.com>
2016-04-13 16:40:57 -0400

    AdminChat

--- src/com/superiornetworks/icarus/ICM_SqlHandler.java
+++ src/com/superiornetworks/icarus/ICM_SqlHandler.java
@@ -30,6 +30,7 @@ public class ICM_SqlHandler
                 + "`loginMessage` TEXT,"
                 + "`rank` TEXT,"
                 + "`ip` VARCHAR(64),"
+                + "`commandSpyOn` BOOLEAN,"
                 + "`godMode` BOOLEAN,"
                 + "`doomHammer` BOOLEAN,"
                 + "`whitelisted` BOOLEAN"
@@ -224,6 +225,20 @@ public class ICM_SqlHandler
             return false;
         }
         Object obj = getFromTable("playerName", playerName, "godMode", "players");
+        if(obj instanceof Boolean)
+        {
+            return (Boolean) obj;
+        }
+        return false;
+    }
+    
+    public static boolean inAC(String playerName) throws SQLException
+    {
+        if(!playerExists(playerName))
+        {
+            return false;
+        }
+        Object obj = getFromTable("playerName", playerName, "commandSpyOn", "players");
         if(obj instanceof Boolean)
         {
             return (Boolean) obj;
